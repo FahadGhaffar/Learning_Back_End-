14527834a36918fdedc01bcb4fd7731616530e9c
Fahad Ghaffar <fahad.ghaffar97@gmail.com>
2022-09-16 13:18:29 -0700

    curd

--- 3-task-manager/controllers/tasks.js
+++ 3-task-manager/controllers/tasks.js
@@ -1,25 +1,56 @@
 
 const Task = require("../models/Task")
 
-const getAllTasks = (req, res) => {
+const getAllTasks = async (req, res) => {
 
+    try {
+        const tasks = await Task.find({});
+        res.status(200).json({ tasks })
 
-    res.send("Get All Task")
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
+
 
 }
 
 const createtask = async (req, res) => {
 
-    const task = await Task.create(req.body);
-    res.status(201).json({ task })
 
+    try {
+        const task = await Task.create(req.body);
+        // const task = new Task({ "name": "ABC", 'completed': true });
+        // await task.save()
+        res.status(201).json({ task })
+
+    } catch (error) {
+
+
+        res.status(500).json({ msg: error })
+
+    }
+
+    // const task = req.body;
+    // res.status(201).json({ task })
 }
 
 
-const gettask = (req, res) => {
+const gettask = async (req, res) => {
 
+    try {
 
-    res.json({ id: req.params.id })
+        const { id: taskID } = req.params;
+        const task = await Task.findOne({ _id: taskID });
+        if (!task) {
+
+            return res.status(404).json({ msg: `No task with id : ${taskID}` })
+        }
+
+        // res.json({ id: req.params.id })
+        res.status(200).json({ task })
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
 
 }
 
@@ -30,10 +61,27 @@ const updatetask = (req, res) => {
 
 }
 
-const deletetask = (req, res) => {
+const deletetask = async (req, res) => {
+
+    try {
+        const { id: taskID } = req.params;
+        const task = await Task.findOneAndDelete({ _id: taskID });
+        if (!task) {
+
+            return res.status(404).json({ msg: `No task with id : ${taskID}` })
+        }
+
+        // res.json({ id: req.params.id })
+        res.status(200).json({ task })
 
 
-    res.send("Delete Task")
+
+
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
+
+    // res.send("Delete Task")
 
 }
 
--- 3-task-manager/models/Task.js
+++ 3-task-manager/models/Task.js
@@ -2,8 +2,17 @@ const mangoose = require('mongoose');
 
 
 const TaskSchema = new mangoose.Schema({
-    name: String,
-    completed: Boolean,
+    name: {
+        type: String,
+        require: [true, "Must Provide name"],
+        trim: true,
+        maxlength: [20, "Name Can not be more than 20 character"]
+    },
+    completed: {
+        type: Boolean,
+        default: false,
+
+    }
 
 
 })

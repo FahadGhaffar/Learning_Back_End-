6afa86f6e42457beb49d6ebcc5db560739921fb1
Fahad Ghaffar <fahad.ghaffar97@gmail.com>
2022-09-21 08:35:12 -0700

    serch query

--- 4-store-api/controllers/products.js
+++ 4-store-api/controllers/products.js
@@ -8,8 +8,20 @@ const getAllProductsStatic = async (req, res) => {
 
 
 const getAllProducts = async (req, res) => {
-
-    const product = await Product.find(req.query);
+    const { feature, company, name } = req.query
+    const queryObject = {}
+    console.log(feature);
+    if (feature) {
+        queryObject.feature = feature === 'true' ? true : false;
+    }
+    if (company) {
+        queryObject.company = company
+    }
+    if (name) {
+        queryObject.name = { $regex: name, $option: 'i' }
+    }
+    console.log(queryObject)
+    const product = await Product.find(queryObject);
     console.log(req.query)
     await res.status(200).json({ product, nbHits: product.length })
 }

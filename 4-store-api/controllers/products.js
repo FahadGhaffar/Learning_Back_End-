d0f3c469ed7ab26aebef727cf2f398767b626fce
FahadGhaffar <fahad.ghaffar97@gmail.com>
2022-10-10 21:49:51 +0500

    sort,selectField,skil,limit

--- 4-store-api/controllers/products.js
+++ 4-store-api/controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require("../model/product")
 
 const getAllProductsStatic = async (req, res) => {
-    const product = await Product.find({ "feature": false, })
+    const product = await Product.find({}).sort('-name -price')
     await res.status(200).json({ product, nbHits: product.length });
 }
 // Just for commit
@@ -10,9 +10,9 @@ const getAllProductsStatic = async (req, res) => {
 
 
 const getAllProducts = async (req, res) => {
-    const { feature, company, name } = req.query
+    const { feature, company, name, sort, field } = req.query
     const queryObject = {}
-    console.log(feature);
+    // console.log(feature);
     if (feature) {
         queryObject.feature = feature === 'true' ? true : false;
     }
@@ -22,9 +22,30 @@ const getAllProducts = async (req, res) => {
     if (name) {
         queryObject.name = { $regex: name, $option: 'i' }
     }
-    console.log(queryObject)
-    const product = await Product.find(queryObject);
-    console.log(req.query)
+    // console.log(queryObject)
+    // const product = await Product.find(queryObject);
+    // console.log(req.query)
+    let result = Product.find(queryObject);
+    if (sort) {
+        const sortList = sort.split(",").join(' ')
+        result = result.sort(sortList);
+
+    }
+    else {
+        result = result.sort("createdAt");
+    }
+    if (field) {
+        const fieldList = field.split(",").join(' ')
+        result = result.select(fieldList);
+
+    }
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit)
+
+    const product = await result
     await res.status(200).json({ product, nbHits: product.length })
 }
 

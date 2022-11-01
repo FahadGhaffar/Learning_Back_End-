879f36e44ad2ce25c8d8c8a976e80d170b9fec2f
Fahad Ghaffar <fahad.ghaffar97@gmail.com>
2022-11-01 12:13:57 -0700

    hide

--- 5-Jwt-basics/controllers/main.js
+++ 5-Jwt-basics/controllers/main.js
@@ -1,10 +1,16 @@
 const { model } = require("mongoose")
 
+const CustomAPIError = require('../errors/custom-error');
 
 const login = async (req, res) => {
 
 
     const { username, password } = await req.body
+
+
+    if (!username || !password) {
+        throw new CustomAPIError("please provide email and password", 400)
+    }
     console.log(username, password);
     console.log(req.body)
     res.send('Fake Login/Register/Signup Route')
--- 5-Jwt-basics/middlerware/error-handler.js
+++ 5-Jwt-basics/middlerware/error-handler.js
@@ -1,9 +1,10 @@
 
+const { StatusCodes } = require('http-status-codes');
 const CustomAPIError = require('../errors/custom-error')
 const errorHandlerMiddleware = async (err, req, res, next) => {
     if (err instanceof CustomAPIError) {
         console.log(err);
-        return res.status(500).json({ msg: "Something went wrong, please try again " })
+        return res.status(err.statusCode).json({ msg: err.message })
     }
     return res.status(500).send('something went wrong try again later ')
 

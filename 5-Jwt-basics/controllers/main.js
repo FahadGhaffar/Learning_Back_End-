2725c0b0e7ed3166249ee167496fcef49f14b5df
Fahad Ghaffar <fahad.ghaffar97@gmail.com>
2022-11-01 13:06:57 -0700

    jwt

--- 5-Jwt-basics/controllers/main.js
+++ 5-Jwt-basics/controllers/main.js
@@ -1,5 +1,7 @@
 const { model } = require("mongoose")
 
+const jwt = require('jsonwebtoken')
+
 const CustomAPIError = require('../errors/custom-error');
 
 const login = async (req, res) => {
@@ -11,8 +13,13 @@ const login = async (req, res) => {
     if (!username || !password) {
         throw new CustomAPIError("please provide email and password", 400)
     }
+
+    const id = new Date().getDate()
+
+
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, { expiresIn: '30d' })
     console.log(username, password);
-    console.log(req.body)
+    console.log(token)
     res.send('Fake Login/Register/Signup Route')
 
 }

366d6983b60f667e7513273282292b5848f3df15
Fahad Ghaffar <fahad.ghaffar97@gmail.com>
2022-11-17 23:13:06 -0800

    router

---
+++ 6-jobs-api/controllers/jobs.js
@@ -0,0 +1,34 @@
+
+
+const getAllJobs = (req, res) => {
+
+    res.send("getAllJobs")
+}
+
+
+const getJob = (req, res) => {
+
+    res.send("getJob")
+}
+
+const createJob = (req, res) => {
+
+    res.send("createJob")
+}
+
+const updateJob = (req, res) => {
+
+    res.send("updateJob")
+}
+const deleteJob = (req, res) => {
+
+    res.send("deleteJob")
+}
+
+module.exports = {
+    getAllJobs,
+    getJob,
+    createJob,
+    updateJob,
+    deleteJob
+}
